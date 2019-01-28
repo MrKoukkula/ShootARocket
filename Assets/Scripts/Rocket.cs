dada23aec284c1d1075363cab0f6ab33929c88d0
Pekka Koukkula <pekka.koukkula@genisys.fi>
2019-01-28 15:20:02 +0200

    Rocket flies up

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rocketRigitBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rocketRigitBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        //if (Input.GetKey(KeyCode.Space) && Input.GetKey(KeyCode.S))
+        //{
+        //    print("space and right pressed");
+        //}
+        //else if (Input.GetKey(KeyCode.Space) && Input.GetKey(KeyCode.A))
+        //{
+        //    print("space and left pressed");
+        //}
+        if (Input.GetKey(KeyCode.Space))
+        {
+            //print("Thrusting");
+            rocketRigitBody.AddRelativeForce(Vector3.up);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("Turning left");
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            print("Turning right");
+        }
+    }
+}
