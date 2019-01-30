c2c36f98c6dbaf6cfee33423fcb44fd6e0df8c8c
Pekka Koukkula <pekka.koukkula@genisys.fi>
2019-01-30 15:23:31 +0200

    First level design

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -7,6 +7,7 @@ public class Rocket : MonoBehaviour
 {
     Rigidbody rocketRigitBody;
     [SerializeField] float turningSpeed = 5f;
+    [SerializeField] float thrustSpeed = 10f;
     AudioSource rocketThrustSound;
     // Start is called before the first frame update
     void Start()
@@ -18,13 +19,28 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        Thrust();
+        Rotate();
     }
 
-    private void ProcessInput()
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "friendly")
+        {
+            Debug.Log("ok");
+        } else if (collision.gameObject.tag == "goal")
+        {
+            Debug.Log("You win!");
+        } else
+        {
+            Debug.Log("You died");
+        }
+    }
+
+    private void Thrust()
     {
 
-        if (Input.GetKeyDown(KeyCode.Space)) 
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             rocketThrustSound.volume = 1.0f;
             rocketThrustSound.Play();
@@ -32,8 +48,9 @@ public class Rocket : MonoBehaviour
 
         if (Input.GetKey(KeyCode.Space))
         {
-            rocketRigitBody.AddRelativeForce(Vector3.up);
-        } else
+            rocketRigitBody.AddRelativeForce(Vector3.up * (Time.deltaTime * thrustSpeed));
+        }
+        else
         {
             rocketThrustSound.volume = rocketThrustSound.volume - 0.1f;
             if (rocketThrustSound.volume == 0)
@@ -41,12 +58,12 @@ public class Rocket : MonoBehaviour
                 rocketThrustSound.Stop();
             }
         }
+    }
 
-        //if (Input.GetKeyUp(KeyCode.Space))
-        //{
-        //    rocketThrustSound.volume = 0.0f;
-        //    rocketThrustSound.Stop();
-        //}
+    private void Rotate()
+    {
+
+        rocketRigitBody.freezeRotation = true; // stop environment physics rotation
 
         if (Input.GetKey(KeyCode.A))
         {
@@ -57,5 +74,7 @@ public class Rocket : MonoBehaviour
             //print("Turning right");
             transform.Rotate(-Vector3.forward * (Time.deltaTime * turningSpeed));
         }
+
+        rocketRigitBody.freezeRotation = false; // continue environment physics rotation
     }
 }
